eeb26e45bd076ad279027819539df9f97ff21bd5
adriankrawczyk <adriankrawczyk0@o2.pl>
2025-02-10 22:27:59 +0100

    Consts in camera look at

--- src/js/camera/setCameraLookAt.js
+++ src/js/camera/setCameraLookAt.js
@@ -4,13 +4,17 @@ import { Vector3 } from "three";
 import inBounds from "../util/math/inBounds";
 
 const Y_OFFSET = 4;
+const MIN_Y = 2;
+const MAX_Y = 2.75;
+const MIN_X = -0.7;
+const MAX_X = 0.7;
 
 export default function setCameraLookAt() {
   camera.lookAt(
     new Vector3(
       0,
-      inBounds(Y_OFFSET - mousePosition.y, 2, 2.75),
-      inBounds(-mousePosition.x, -0.7, 0.7)
+      inBounds(Y_OFFSET - mousePosition.y, MIN_Y, MAX_Y),
+      inBounds(-mousePosition.x, MIN_X, MAX_X)
     )
   );
 }
