50691f6dbb3690415dfceab8500fc7329dc6dac2
adriankrawczyk <adriankrawczyk0@o2.pl>
2025-02-10 22:26:42 +0100

    Bounds for camera movement

--- src/js/camera/setCameraLookAt.js
+++ src/js/camera/setCameraLookAt.js
@@ -1,9 +1,16 @@
 import camera from "./camera";
 import mousePosition from "../mouse/mousePosition";
 import { Vector3 } from "three";
+import inBounds from "../util/math/inBounds";
 
 const Y_OFFSET = 4;
 
 export default function setCameraLookAt() {
-  camera.lookAt(new Vector3(0, Y_OFFSET - mousePosition.y, -mousePosition.x));
+  camera.lookAt(
+    new Vector3(
+      0,
+      inBounds(Y_OFFSET - mousePosition.y, 2, 2.75),
+      inBounds(-mousePosition.x, -0.7, 0.7)
+    )
+  );
 }
---
+++ src/js/util/math/inBounds.js
@@ -0,0 +1,5 @@
+export default function inBounds(number, min, max) {
+  if (number < min) return min;
+  else if (number > max) return max;
+  else return number;
+}
