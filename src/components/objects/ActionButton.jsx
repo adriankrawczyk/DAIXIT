00cd5c7ce983a4895f0f999850818f138c5b0d52
adriankrawczyk <adriankrawczyk0@o2.pl>
2025-02-21 11:57:47 +0100

    Buttom fix

--- src/components/objects/ActionButton.jsx
+++ src/components/objects/ActionButton.jsx
@@ -2,18 +2,28 @@ import React, { forwardRef } from "react";
 import TextLabel from "../lobby/util/TextLabel";
 import { Text } from "@react-three/drei";
 
-const ActionButton = forwardRef(({dimensions, color, text, onClick, defaultScale=0}, ref) => {
-  const fontSize = 0.25;
-  return (
-    <mesh ref={ref} position={dimensions} rotation={[-Math.PI / 13, 0, 0]} scale={defaultScale} onClick={(e)=>{
-      e.stopPropagation();
-      onClick(); 
-      }}>
-      <Text scale={fontSize} position={[0,0,0.1]}>{text}</Text>
-      <boxGeometry args={[1, 0.5, 0.1]} />
-      <meshStandardMaterial color={color} />
-    </mesh>
-  );
-});
+const ActionButton = forwardRef(
+  ({ dimensions, color, text, onClick, defaultScale = 0 }, ref) => {
+    const fontSize = 0.25;
+    return (
+      <mesh
+        ref={ref}
+        position={dimensions}
+        rotation={[-Math.PI / 13, 0, 0]}
+        scale={defaultScale}
+        onClick={(e) => {
+          e.stopPropagation();
+          onClick();
+        }}
+      >
+        <Text scale={fontSize} position={[0, 0, 0.02]}>
+          {text}
+        </Text>
+        <boxGeometry args={[1, 0.5, 0.01]} />
+        <meshStandardMaterial color={color} />
+      </mesh>
+    );
+  }
+);
 
 export default ActionButton;
