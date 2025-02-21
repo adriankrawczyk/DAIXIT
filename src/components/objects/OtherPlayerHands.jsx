6b5f79f8c96e9a012fcaa38244b14afc4fcfa2fc
adriankrawczyk <adriankrawczyk0@o2.pl>
2025-02-21 02:24:04 +0100

    cleanup

--- src/components/objects/OtherPlayerHands.jsx
+++ src/components/objects/OtherPlayerHands.jsx
@@ -10,7 +10,9 @@ import { getAnimations } from "../firebase/playerMethods";
 import { addToTable, backToHand } from "../firebase/animations";
 
 const OtherPlayerHand = ({ numberOfCards = 5 }) => {
+  const [otherPlayersData, setOtherPlayersData] = useState([]);
   const [otherPlayerHandsData, setOtherPlayerHandsData] = useState([]);
+  const [processedAnimations, setProcessedAnimations] = useState(new Set());
   const cardsRef = useRef({});
 
   const handleAnimation = (animation, cardRef) => {
@@ -38,6 +40,7 @@ const OtherPlayerHand = ({ numberOfCards = 5 }) => {
   useEffect(() => {
     const fetchData = async () => {
       const players = await getOtherPlayersData();
+      setOtherPlayersData(players);
       setOtherPlayerHandsData(
         await Promise.all(
           players.map((player) => getSetupData(player.position))
