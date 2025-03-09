b90bbad19c980ec6beb1deba08b22c6c0eaf945e
adriankrawczyk <adriankrawczyk0@o2.pl>
2025-03-09 14:52:34 +0100

    Firebase api key in .env

--- src/components/firebase/firebaseConfig.js
+++ src/components/firebase/firebaseConfig.js
@@ -1,16 +1,16 @@
 import { initializeApp } from "firebase/app";
-import { getDatabase, ref, set, get, update } from "firebase/database";
+import { getDatabase } from "firebase/database";
 import { getAuth, signInAnonymously, onAuthStateChanged } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD85eTRocsjNq7BPaEyHU9zwRKjUrZYIxw",
-  authDomain: "daixit-70e54.firebaseapp.com",
-  databaseURL: "https://daixit-70e54-default-rtdb.firebaseio.com",
-  projectId: "daixit-70e54",
-  storageBucket: "daixit-70e54.appspot.com",
-  messagingSenderId: "628264390852",
-  appId: "1:628264390852:web:9a1d730f3c3a82151c3991",
-  measurementId: "G-EVWM9T483Q",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
